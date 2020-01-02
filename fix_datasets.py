9f7e3ec8338daec503ea05cd38d2d0e18079f0d0
fedelux3 <f.luzzi1@campus.unimib.it>
2020-01-02 10:32:38 +0100

    add fix_dataset.py
    serve a creare i dataset con colonne sulle date e delete wrong rows

--- fix_datasets.py
+++ fix_datasets.py
@@ -4,13 +4,21 @@ import os
 #scrivere nella variabile in fondo al codice (variabile dir) il nome della directory
 #il software genera una cartella fixed all'interno della stessa con i files sistemati
 
-#elimina le righe
+#elimina le righe di intestazione sbagliate
 def delete(df): 
     df1 = df.loc[df['timestamp'] == 'timestamp']
     df2 = df.drop(df.index[df1.index])
     return df2
     #df2[df2['timestamp'] == 'timestamp']
-    
+
+#aggiunge le colonne dei componenti della date
+def date_columns(df):
+    df['day'] = df['timestamp'].apply(lambda x: x[0:2])
+    df['month'] = df['timestamp'].apply(lambda x: x[3:5])
+    df['year'] = df['timestamp'].apply(lambda x: x[6:10])
+    df['hour'] = df['timestamp'].apply(lambda x: x[11:13])
+    df['min'] = df['timestamp'].apply(lambda x: x[14:16])
+
 #prende tutti i file nella cartella e gli fa eseguire il delete
 def delete_dir(dir):
     files = os.listdir('.\\' + dir)
@@ -20,9 +28,22 @@ def delete_dir(dir):
     for file in files:
         df = pd.read_csv(dir + file)
         df_fixed = delete(df)
-        df_fixed.to_csv(dir_output + file)
+        df_fixed.to_csv(dir_output + file, index = False)
         print("funziona")
-    
+
+#prende tutti i file nella cartella e gli fa eseguire le funzioni per fixare
+def fix_dir(dir):
+    files = os.listdir('.\\' + dir)
+    dir_output = dir + 'fixed\\'
+    os.mkdir(dir_output)
+
+    for file in files:
+        df = pd.read_csv(dir + file)
+        df_fixed = delete(df)
+        date_columns(df_fixed)
+        df_fixed.to_csv(dir_output + file, index = False)
+        print("fatto")
+
 if __name__ == '__main__':
     #!!!
     #dir è il nome della cartella in cui devo fixare i dati
@@ -30,6 +51,5 @@ if __name__ == '__main__':
     # - non ci siano altri dati se non i csv
     # - mettere i due \\ in fondo alla stringa
     #se no non funziona
-    dir = '26.12.19 csv files (fede) [full day]\\'
-    delete_dir(dir)
-    print('cancellato')
+    dir = '24.12.19 09_00 csv files\\'
+    fix_dir(dir)
