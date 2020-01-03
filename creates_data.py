c79f1684b5125f0574944294ef8aa597006d60fc
Chri1629 <christian.uccheddu@gmail.com>
2020-01-03 14:09:09 +0100

    now imports file in the correct way

--- creates_data.py
+++ creates_data.py
@@ -3,12 +3,20 @@
 import pandas as pd 
 import argparse
 import numpy as np
+import glob, os
 
 
+#   Inizio con l'aprire tutti i dati presenti con nella directory data:
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--data', type=str, required=False, help="Inserire la directory da cui prendere i dati", default="data")
 
 
 args = parser.parse_args()
+data = os.chdir(args.data)
 
-data = np.load(args.data)
+# Mi occupo ora di importarli nella maniera corretta in pandas.
+
+for file in glob.glob("*.csv"):
+    file =  pd.read_csv(file)
+
+print(file.head())
