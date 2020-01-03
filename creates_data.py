acd0389446f5e1a7f734513cf6c07843e5c3097f
Chri1629 <christian.uccheddu@gmail.com>
2020-01-03 17:48:29 +0100

    fix

--- creates_data.py
+++ creates_data.py
@@ -17,32 +17,39 @@ data = os.chdir(args.data)
 # Mi occupo ora di importarli nella maniera corretta in pandas. Per prima cosa mette tutti i file in una lista e poi li importa
 # come dataframe pandas. Questo funziona bene quando ho una directory sola.
 '''
-files = os.listdir(data)
+def conc_dataframe():
+    files = os.listdir(data)
 
-# Uso la lista dfs per salvare i data frame pandas, una volta messi dentro posso concatenarli.
-dfs = []
-files_csv = [i for i in files if i.endswith('.csv')]
+    # Uso la lista dfs per salvare i data frame pandas, una volta messi dentro posso concatenarli.
+    dfs = []
+    files_csv = [i for i in files if i.endswith('.csv')]
 
 
-for file in files_csv:
-    dfs.append(pd.read_csv(file))  
+    for file in files_csv:
+        dfs.append(pd.read_csv(file))  
 
-#   Adesso faccio la concatenazione dei dataframe presenti nella lista
-df = pd.concat(dfs)
+    #   Adesso faccio la concatenazione dei dataframe presenti nella lista
+    df = pd.concat(dfs)
 '''
 
 # Provo a fare la stessa cosa ma avendo file in più directory, non capisco perchè ma non trova i file quando devono essere concatenati
-files_csv = []
-dfs = []
-dyr = os.listdir(data)
+def conc_dataframe():
+    files_csv = []
+    dfs = []
+    dyr = os.listdir(data)
 
 
-for directory in dyr:
-    files = os.listdir(directory)
-    for i in files:
-        if i.endswith('.csv'):
-            files_csv.append(i)
-            #dfs.append(pd.read_csv(i))
-            #df = pd.concat(dfs)  
+    for directory in dyr:
+        files = os.listdir(directory)
+        for i in files:
+            if i.endswith('.csv'):
+                #files_csv.append(i)
+                #dfs.append(pd.read_csv(i))
+                print(i)
+                #pd.read_csv(i)
+                #df = pd.concat(dfs)  
 
-print(files_csv)
+    #print(files_csv)
+
+if __name__ == '__main__':
+    conc_dataframe()
