04603803737c4588e86134d2cf1873fae6d345be
MarcoP9 <m.peracchi1@campus.unimib.it>
2020-01-22 12:09:15 +0100

    Cleaning and reorganizing code of kafka

--- kafka_generator/scraper_consumer.py
+++ kafka_generator/scraper_consumer.py
@@ -5,9 +5,11 @@ import requests, sys, time, os, argparse
 import sched
 import json
 
+
+#########################
 # Set time scheduler
 scheduler = sched.scheduler(time.time, time.sleep)
-#set global variables
+# Inizializzazione delle variabili necessarie
 output_dir = "output/"
 api_key = ""
 country_codes = ""
@@ -19,42 +21,47 @@ snippet_features = ["title",
                    "channelId",
                    "channelTitle",
                    "categoryId"]
+#########################
+
 
 # Any characters to exclude, generally these are things that become problematic in CSV files
 unsafe_characters = ['\n', '"']
 
-######################
-# Definisco il consumer
-consumer = KafkaConsumer(
-  bootstrap_servers=["kafka:9092"],
-  auto_offset_reset="latest",
-  value_deserializer=lambda v: json.loads(v.decode("utf-8")))
-# sottoscrive un determinato topic
-consumer.subscribe(["youtube_video"])
 
+
+#########################
+# Funzione di lettura del file txt
 def setup(code_path):
     with open(code_path) as file:
         country_codes = [x.rstrip() for x in file]
 
     return country_codes
+#########################
 
+
+#########################
 def prepare_feature(feature):
     # Removes any character from the unsafe characters list and surrounds the whole item in quotes
     for ch in unsafe_characters:
         feature = str(feature).replace(ch, "")
     return f'"{feature}"'
+#########################
+
 
 #def get_tags(tags_list):
     # Takes a list of tags, prepares each tag and joins them into a string by the pipe character
  #   return prepare_feature("|".join(tags_list))
 
+
+#########################
+# La funzione get_videos prende i file json letti da kafka e restituisce una lista contenente dizionari con le informazioni di ogni video
 def get_videos(videos):
     lines = []
     items = videos.value['items']
     service = videos.value['service']
     country = service['country']
     timestamp = service['timestamp']
-    
+    # Il ciclo for passa attraverso tutte i video contenuti dentro items
     for video in items:
         comments_disabled = False
         ratings_disabled = False
@@ -94,27 +101,59 @@ def get_videos(videos):
         lines.append(line)
         
     return lines
+#########################
 
+
+#########################
+# La funzione legge tutti i dati memorizzati all'interno di kafka e li inserisce in un database mongo
 def get_data():
-    #video è un json fatto con i suoi campi
+    # i file video sono json con diversi campi, tra cui un campo items che contiene i vari video in trending
     for video in consumer:
         #qua va fatto il parser del consumer
         l_video = get_videos(video)
-        #inserisco i documenti in mongo
+        # la funzione inserisce i documenti salvati dentro alla lista l_video in mongo
         insert_video_mongo(l_video)
         #stampo i json nella cartella output_consumed
         #write_to_file(l_video, i)
+#########################
 
+
+#########################
+# La funzione consume legge da txt i paesi richiesti e lancia la lettura dei dati
 def consume():
     global output_dir
     global country_codes
     global dt
     
     country_code_path = "country_codes.txt"
-
+	# Lettura del file dei paesi richiesti
     country_codes = setup(country_code_path)
-   
-    get_data()
 
-#fa partire il consume
+    get_data()
+#########################
+
+
+######################
+# Definisco il consumer
+consumer = KafkaConsumer(
+  bootstrap_servers=["kafka:9092"],
+  auto_offset_reset="latest",
+  value_deserializer=lambda v: json.loads(v.decode("utf-8")))
+# sottoscrive un determinato topic
+consumer.subscribe(["youtube_video"])
+#########################
+
+
+
+#########################
 consume()
+#########################
+
+
+
+
+
+
+
+
+
