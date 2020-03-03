01f73a285afcbc4777237ee05081d978596c0f01
Chri1629 <christian.uccheddu@gmail.com>
2020-03-03 11:43:48 +0100

    Now bot work

---
+++ bot/function.py
@@ -0,0 +1,17 @@
+import telegram
+
+my_token = '986806970:AAFLUAE9jj5jdlo9Nw9bvgGWkZdlquSz4mw'
+group_id = '-284013422'
+chat_id = '172982427'
+
+def send(chat_id, token=my_token):
+	msg = 'Ci sono problemi nella presa dati' 
+	bot = telegram.Bot(token=token)
+	bot.sendMessage(chat_id=group_id, text=msg)
+
+
+def update(chat_id, token=my_token):	
+	bot = telegram.Bot(token=token)
+	updates = bot.get_updates()
+	print([u.message.text for u in updates])
+	
---
+++ bot/main.py
@@ -0,0 +1,5 @@
+from function import *
+
+if __name__ == '__main__':
+	send(group_id, token=my_token)
+	update(group_id, token=my_token)
