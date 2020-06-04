6d4423b912261f3549f4f0ed363d5f5cf1bdbd2d
fedelux3 <f.luzzi1@campus.unimib.it>
2020-06-04 12:05:48 +0200

    clean json_to_mongo scripts

--- clean_store_data/json_to_mongo (windows)/json_to_mongo.py
+++ clean_store_data/json_to_mongo (windows)/json_to_mongo.py
@@ -4,49 +4,69 @@ import os
 import json
 
 def list_directory(data):
+    '''
+    Estrae lista di directory nelle quali ho i file json
+    @params:
+        data:   - Required   : cartella di sottocartelle di file json
+    '''
     l = []
     print(data)
     for x in os.listdir(data + "\\"):
         l.append(x)
-            #print(x)
+        
     return l
 
 if __name__ == '__main__':
-    # ottengo la directory in cui applicare la ricerca
+    '''
+    @params:
+        -d: directory di directories dei json file
+        -u: mongoDB user
+        -p: mongoDB password
+        -port: mongoDB porta in cui comunica
+        -db: nome database mongodb
+        -c: nome collezione mongodb in cui immagazzinare i dati
+    '''
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--data', type=str, required=False, help="Inserire la directory da cui prendere i dati", default="data")
     parser.add_argument('-u', '--user', type=str, required=False, help="Inserire l'user mongodb", default="")
     parser.add_argument('-p', '--password', type=str, required=False, help="Inserire la password mongodb", default="")
     parser.add_argument('-port', '--port', type=str, required=False, help="Inserire la porta con cui dialogare con mongodb", default=27017)
     parser.add_argument('-db', '--database', type=str, required=False, help="Inserire il database mongo di output", default="YT_data")
-    parser.add_argument('-c', '--collection', type=str, required=False, help="Inserire la mongodb di output", default="videos_march")
+    parser.add_argument('-c', '--collection', type=str, required=False, help="Inserire la mongodb di output", default="video")
     
     args = parser.parse_args()
-    dir_main = args.data
-    # definizione del client mongo da utilizzare
-    client = MongoClient('localhost', args.port, username = args.user, password = args.password)
-    db = client[args.database]
-    col = db[args.collection]
+    try:
+        dir_main = args.data
+        list_videos = [] # lista in cui salva i video da caricare su mongo
+        l_dir = list_directory(dir_main) # lista di directory da cui prende i file mongo
+        print(l_dir)
+    except:
+        print("error json directory path")
+        exit()
     
-    list_videos = [] # lista in cui salvo i video da caricare su mongo
-    l_dir = list_directory(dir_main) # lista di directory da cui prendo i file mongo
-    print(l_dir)
+    try:
+        user = args.user
+        password = args.password
+        # definizione del client mongo da utilizzare
+        client = MongoClient('localhost', args.port, username = args.user, password = args.password)
+        db = client[args.database] # connessione al db
+        col = db[args.collection] # connesione alla collection
+    except:
+        print("error mongo connection")
+        exit()
 
-    for directory in l_dir: #per ogni cartella
+    for directory in l_dir: #per ogni cartella nella lista
         path = dir_main + "\\" + directory + "\\"
         files = os.listdir(path)
         
         for file in files: #per ogni file json nella cartella
+            # apre il file se .json
             if file.endswith('.json'):
-
                 with open(path + file, "r") as read_file:
                     j_file = json.load(read_file)
-                
-                list_videos.extend(j_file) # aggiunge gli elementi alla lista
-        ##############################################
-        #merge_videos(list_videos, covid)
-        #############################################
-        # carico la lista di dizionari nella collezione del database mongo specificate
+                # appende alla lista i dizionari dei video del json
+                list_videos.extend(j_file) 
+        # carica la lista di dizionari nella collezione del database mongo specificate
         col.insert_many(list_videos)
         list_videos = []
         print("directory " + directory + " correctly uploaded on mongoDB")
--- clean_store_data/json_to_mongo (windows)/merge_to_mongo.py
+++ clean_store_data/json_to_mongo (windows)/merge_to_mongo.py
@@ -5,53 +5,60 @@ import json
 import pandas as pd
 import time
 
-# Print iterations progress
 def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
     """
-    Call in a loop to create terminal progress bar
+    Chiamato nel ciclo per stampare barra di progressione
     @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+        iteration   - Required  : iterazione corrente (Int)
+        total       - Required  : iterazioini totali (Int)
+        prefix      - Optional  : prefisso stringa (Str)
+        suffix      - Optional  : suffisso string (Str)
+        decimals    - Optional  : numero di cifre decimali nella percentuale (Int)
+        length      - Optional  : lunghezza della barra (Int)
+        fill        - Optional  : carattere riempitore della barra (Str)
+        printEnd    - Optional  : carattere di chiusura (e.g. "\r", "\r\n") (Str)
     """
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filledLength = int(length * iteration // total)
     bar = fill * filledLength + '-' * (length - filledLength)
     print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = printEnd)
-    # Print New Line on Complete
+    # stampa una linea nuova al termine
     if iteration == total: 
         print()
 
 
 
 def list_directory(data):
+    '''
+    Estrae lista di directory nelle quali ho i file json
+    @params:
+        data:   - Required   : cartella di sottocartelle di file json
+    '''
     l = []
     print(data)
     for x in os.listdir(data + "\\"):
         l.append(x)
-            #print(x)
     return l
 
-#funzione di merge
-#input: -videos (list of dictionary) -covid (dataframe)
 def merge_videos(videos, covid):
-    video_covid = videos #output (list of dictionary)
+    '''
+    Funzione di merge tra 
+    @params:
+        videos:     Youtube videos (lista dizionari)
+        covid:      covid data (dataframe)
 
+    '''
+    # init barra di progressione
     tot = len(videos)
-    #per barra progressione
     printProgressBar(0, tot, prefix = 'Progress:', suffix = 'Complete', length = 50)
-    for  i, video in enumerate(video_covid): #passa tutti i video
+
+    for  i, video in enumerate(videos): #passa tutti i video
         date = video["trending_date"]
         c_name = video["country_name"]
-        #cercare in covid date e c_name
+        # cerca in covid date e c_name del video corrente
         row = covid.loc[(covid["date"] == date) & (covid["location"] == c_name)]
-        #aggiungo i campi
-        #print(video)
+        # estrae le informazioni covid di interesse
+        # le immagazzina in un dizionario
         d = {
             "cases_tot" : int(row["total_cases"].values[0]),
             "cases_new" : int(row["new_cases"].values[0]),
@@ -59,40 +66,61 @@ def merge_videos(videos, covid):
             "deaths_new" : int(row["new_deaths"].values[0]),
             "country_population" : int(row["population"].values[0])
             }
-
+        # aggiunge il dizionario al video
         video["covid"] = d
-
-        #video["covid_cases_tot"] = int(row["total_cases"].values[0])
-        #video["covid_cases_new"] = int(row["new_cases"].values[0])
-        #video["covid_deaths_tot"] = int(row["total_deaths"].values[0])
-        #video["covid_deaths_new"] = int(row["new_deaths"].values[0])
-        #video["country_population"] = int(row["population"].values[0])
         
         # Update Progress Bar
         printProgressBar(i + 1, tot, prefix = 'Progress:', suffix = 'Complete', length = 50)
 
 if __name__ == '__main__':
+    '''
+    @params:
+        -d: directory di directories dei json file
+        -dc: path file dati covid
+        -u: mongoDB user
+        -p: mongoDB password
+        -port: mongoDB porta in cui comunica
+        -db: nome database mongodb
+        -c: nome collezione mongodb in cui immagazzinare i dati
+    '''
+    # start timer
     time_start = time.clock()
-    # ottengo la directory in cui applicare la ricerca
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--data', type=str, required=False, help="Inserire la directory da cui prendere i dati", default="data")
+    parser.add_argument('-d', '--data', type=str, required=False, help="Inserire la directory da cui ho directories json", default="data")
+    parser.add_argument('-dc', '--data_covid', type=str, required=False, help="Inserire il path del .csv covid", default="covid_data.csv")
     parser.add_argument('-u', '--user', type=str, required=False, help="Inserire l'user mongodb", default="")
     parser.add_argument('-p', '--password', type=str, required=False, help="Inserire la password mongodb", default="")
     parser.add_argument('-port', '--port', type=str, required=False, help="Inserire la porta con cui dialogare con mongodb", default=27017)
-    parser.add_argument('-db', '--database', type=str, required=False, help="Inserire il database mongo di output", default="YT_data")parser.add_argument('-db', '--database', type=str, required=False, help="Inserire la directory da cui prendere i dati", default="YT_data")
-    parser.add_argument('-c', '--collection', type=str, required=False, help="Inserire la collection in cui immagazzinare i dati", default="try_merge_2")
+    parser.add_argument('-db', '--database', type=str, required=False, help="Inserire il database mongo di output", default="YT_data")
+    parser.add_argument('-c', '--collection', type=str, required=False, help="Inserire la collection in cui immagazzinare i dati", default="video")
     
     args = parser.parse_args()
-    dir_main = args.data
-    # definizione del client mongo da utilizzare
-    client = MongoClient('localhost', args.port, username = args.user, password = args.password)
-    db = client[args.database]
-    col = db[args.collection]
-    df_covid = pd.read_csv("covid_data.csv")
+    try:
+        dir_main = args.data
+        l_dir = list_directory(dir_main) # lista di directory da cui prendo i file mongo
+        print(l_dir)
+    except:
+        print("error json directory path")
+        exit()
+
+    try:
+        user = args.user
+        password = args.password
+        # definizione del client mongo da utilizzare
+        client = MongoClient('localhost', args.port, username = user, password = password)
+        db = client[args.database] # connessione al db
+        col = db[args.collection] # connessione alla collection
+    except:
+        print("error mongo connection")
+        exit()
+    
+    try:
+        df_covid = pd.read_csv(args.data_covid) # lettura dati covid
+    except:
+        print("error covid data path")
+        exit()
 
     list_videos = [] # lista in cui salvo i video da caricare su mongo
-    l_dir = list_directory(dir_main) # lista di directory da cui prendo i file mongo
-    print(l_dir)
 
     for directory in l_dir: #per ogni cartella
         path = dir_main + "\\" + directory + "\\"
@@ -100,17 +128,16 @@ if __name__ == '__main__':
         
         for file in files: #per ogni file json nella cartella
             if file.endswith('.json'):
-
+                # apre il file se .json
                 with open(path + file, "r") as read_file:
                     j_file = json.load(read_file)
-                
+                # appende alla lista i dizionari dei video del json
                 list_videos.extend(j_file) # aggiunge gli elementi alla lista
         print("upload files")
-        ##############################################
         print(directory + " merging ...")
-        merge_videos(list_videos, df_covid)
+        # merging function
+        merge_videos(list_videos, df_covid) 
         print("merge_fatto")
-        #############################################
         # carico la lista di dizionari nella collezione del database mongo specificate
         col.insert_many(list_videos)
         list_videos = []
@@ -118,4 +145,4 @@ if __name__ == '__main__':
 
     print("files json correctly uploaded on mongoDB !!!")
     time_elapsed = (time.clock() - time_start)
-    print("computaion time: " + str(time_elapsed))
+    print("computation time: " + str(time_elapsed))
