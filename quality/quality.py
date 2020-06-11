98cf4cb01c36f3172ca43755df7b0b7bea89eca5
Chri1629 <christian.uccheddu@gmail.com>
2020-06-11 23:28:59 +0200

    Fix all the quality plots

--- quality/quality.py
+++ quality/quality.py
@@ -26,93 +26,51 @@ def correlation_plot(data, out_dir):
     fig, ax = plt.subplots(figsize=(11, 9))
 
     # Generate a custom diverging colormap
-    cmap = sns.diverging_palette(h_neg=130, h_pos=10, s=99, l=55, sep=3, as_cmap=True)
+    cmap = sns.diverging_palette(220, 20, sep=20, as_cmap=True)
 
     # Draw the heatmap with the mask and correct aspect ratio
     
     sns.heatmap(corr, cmap=cmap,  annot=True, 
             linewidths=1.3, linecolor='black', cbar=True, ax=ax)
     plt.yticks(rotation = 0)
+    plt.title("Correlazioni tra le variabili", size = 30, pad = 20)
     plt.show()
     fig.savefig(out_dir + '/risposte_correlation_plot.png', bbox_inches='tight', dpi = 600)
 
-def box_plot(data, out_dir):
+def box_plot_1(data, out_dir):
     '''
-    Disegna un box plot:
+    Disegna il box plot della prima infografica
     @params:
-        data:   dati di cui viene disegnato il box plot
+        data:   Dati di cui disegnare i violin plot.
     '''
-    plt.style.use('seaborn')
-    fig, ax= plt.subplots(figsize=(15, 8))
-
-    bp = ax.boxplot(data, sym='k+', 
-                notch=True, bootstrap=5000, patch_artist=True)
-    ax.yaxis.grid(True, linestyle='-', which='major', color='lightgrey',
-               alpha=0.5)
-
-    ax.set_axisbelow(True)
-    ax.set_title('Visualizzazione della questionario di qualità')
-    ax.set_xlabel('Distribuzione')
-    ax.set_ylabel('Valori')
-    ax.set_ylim(0,6.5)
-    ax.set_xticklabels(['chiarezza', 'utilità', 'bellezza', 'intuitività', 'informatività','totale'],
-                    rotation=45, fontsize=8)
-    
-
-    colors = [(0.2,0.1,0.3), (0.9,0.8,0.4), (0.2,0.3,0.7), (0.7,0.2,0.4), (0.5,0.3,0.2), (0.3,0.2,0.5)]
-
-    for box, color in zip(bp['boxes'], colors):
-        box.set(color= color, linewidth=2)
-        box.set(facecolor = color )
-        box.set(hatch = '/')
-
+    data = data.melt(var_name='Categories', value_name='Vote')
+    sns.boxplot(x="Categories", y="Vote",data = data, palette="Set3", notch = True)
+    plt.grid()
+    plt.title("Risposte della prima infografica", pad = 20)
     plt.show()
-    fig.savefig(out_dir + '/risposte_box_plot.png', bbox_inches='tight', dpi = 600)
+    plt.savefig(out_dir + '/risposte_box_plot_first.png', bbox_inches='tight', dpi = 600)
 
-def violin_plot(data, out_dir):
+def box_plot_2(data, out_dir):
     '''
-    Disegna un violin plot dei dati:
+    Disegna il box plot della prima infografica
     @params:
-        data:  dati di cui viene disegnato il violin plot 
+        data:   Dati di cui disegnare i violin plot.
     '''
-    fig, ax= plt.subplots(figsize=(15, 8))
+    data = data.melt(var_name='Categories', value_name='Vote')
+    sns.boxplot(x="Categories", y="Vote",data = data, palette="Set3", notch = True)
+    plt.grid()
+    plt.title("Risposte della seconda infografica", pad = 20)
+    plt.show()  
+    plt.savefig(out_dir + '/risposte_box_plot_second.png', bbox_inches='tight', dpi = 600)
 
-    bp = ax.violinplot(data, showmeans=True, showmedians=True,
-        showextrema=True)
-    ax.yaxis.grid(True, linestyle='-', which='major', color='lightgrey',
-               alpha=0.5)
-
-    pos   = [1, 2, 3,4,5,6]
-    label = ['chiarezza', 'utilità', 'bellezza', 'intuitività', 'informatività','totale']
-    ax.set_axisbelow(True)
-    ax.set_title('Visualizzazione della questionario di qualità')
-    ax.set_xlabel('Distribuzione')
-    ax.set_ylabel('Valori')
-    ax.set_xticks(pos)
-    ax.set_xticklabels(label)
-    ax.set_ylim(0,6.5)
-    # Non so perché ma scala le etichette dei valori a destra di 1, quindi inserisco un valore prima in modo da averli tutti
-    
-
-    colors = [(0.2,0.1,0.3), (0.9,0.8,0.4), (0.2,0.3,0.7), (0.7,0.2,0.4), (0.5,0.3,0.2), (0.3,0.2,0.5)]
-
-    for pc, color in zip(bp['bodies'],colors):
-        pc.set_facecolor(color)
-        pc.set_edgecolor(color)
-        pc.set_alpha(1)
-
-    plt.show()
-    fig.savefig(out_dir + '/risposte_violin_plot.png', bbox_inches='tight', dpi = 600)
-
-def scatter_plot(x, y, out_dir):
+def scatter_plot_1(x, y, out_dir):
     '''
-    Disegna uno scatter plot dei dati:
+    Disegna uno scatter plot per il percepito della seconda infografica:
     @params:
         x:  Prima grandezza da confrontare 
         y:  Seconda grandezza da confrontare
     '''
-        
-    fig = plt.figure(figsize=(15,8))
+    
     plt.plot(x,y, '+', color = (0.2,0.1,0.3))
 
     
@@ -125,9 +83,36 @@ def scatter_plot(x, y, out_dir):
     plt.xlabel('Dichiarato', size = 35) 
     plt.ylabel('Calcolato', size = 35) 
     plt.legend(loc="best", prop={'size': 15})
+    plt.title("Scatter plot seconda infografica", size = 35)
     plt.show()
     plt.ioff()
-    fig.savefig(out_dir + '/risposte_scatter_plot.png', bbox_inches='tight', dpi = 600)
+    plt.savefig(out_dir + '/risposte_scatter_plot_first.png', bbox_inches='tight', dpi = 600)
+
+
+def scatter_plot_2(x, y, out_dir):
+    '''
+    Disegna uno scatter plot per il percepito della seconda infografica:
+    @params:
+        x:  Prima grandezza da confrontare 
+        y:  Seconda grandezza da confrontare
+    '''
+        
+    plt.plot(x,y, '+', color = (0.2,0.1,0.3))
+
+
+    #Plot of the bisector, the line in which the poinst must be in the neighborhood
+    plt.plot([0,6],[0,6], "r--", color = "deepskyblue", label = "Perfect Correlation")
+    plt.xticks( fontsize = 20)
+    plt.yticks( fontsize = 20)
+    plt.xlim(0,6)
+    plt.ylim(0,6)
+    plt.xlabel('Dichiarato', size = 35) 
+    plt.ylabel('Calcolato', size = 35) 
+    plt.legend(loc="best", prop={'size': 15})
+    plt.title("Scatter plot seconda infografica", size = 35)
+    plt.show()
+    plt.ioff()
+    plt.savefig(out_dir + '/risposte_scatter_plot_second.png', bbox_inches='tight', dpi = 600)
 
 def quality(out_dir):
     '''
@@ -139,46 +124,50 @@ def quality(out_dir):
     terzo_coefficiente = 0.190
     quarto_coefficiente = 0.174
     quinto_coefficiente = 0.151
-    risposte = pd.read_csv("risposte.csv")
+    risposte_prima_info = pd.read_csv("risposte_prima_info.csv")
+    risposte_seconda_info = pd.read_csv("risposte_seconda_info.csv")
 
     # Rename the columns.
 
-    risposte.columns = ['time_stemp', 'chiarezza', 'utilità', 'bellezza', 'intuitività', 'informatività','totale']
+    risposte_prima_info.columns = ['timestamp', 'chiarezza', 'utilita', 'bellezza', 'intuitivita', 'informativita','totale']
+    risposte_seconda_info.columns = ['timestamp', 'chiarezza', 'utilita', 'bellezza', 'intuitivita', 'informativita','totale']
 
     # Drop the column time_stemp
 
-    risposte = risposte.drop('time_stemp', axis = 1)
-    print(risposte)
-    risposte['percepito'] = primo_coefficiente*risposte['chiarezza'] + secondo_coefficiente*risposte['utilità'] + terzo_coefficiente*risposte['bellezza'] 
-    + quarto_coefficiente*risposte['intuitività'] + quinto_coefficiente*risposte['informatività']
+    risposte_prima_info = risposte_prima_info.drop('timestamp', axis = 1)
+    risposte_seconda_info = risposte_seconda_info.drop('timestamp', axis = 1)
 
+    
+    risposte_prima_info['percepito'] = primo_coefficiente*risposte_prima_info['chiarezza'] + secondo_coefficiente*risposte_prima_info['utilita'] + terzo_coefficiente*risposte_prima_info['bellezza'] 
+    + quarto_coefficiente*risposte_prima_info['intuitivita'] + quinto_coefficiente*risposte_prima_info['informativita']
+
+    risposte_seconda_info['percepito'] = primo_coefficiente*risposte_seconda_info['chiarezza'] + secondo_coefficiente*risposte_seconda_info['utilita'] + terzo_coefficiente*risposte_seconda_info['bellezza'] 
+    + quarto_coefficiente*risposte_seconda_info['intuitivita'] + quinto_coefficiente*risposte_seconda_info['informativita']
+    
     # Creates the new summarize column
+ 
     
-    print('-----------------------------------------------------')
-    a = pd.DataFrame(round(risposte.describe(),2))
-    print(a)
-    dati = [risposte['chiarezza'], risposte['utilità'], risposte['bellezza'], risposte['intuitività'], risposte['informatività'], risposte['totale']]
-
-    # Implemento anche l'indice R^2 per vedere come si comporta.
-    
-    
+    dati_prima_info = risposte_prima_info[['chiarezza','utilita','bellezza','intuitivita','informativita','totale']]
+    dati_seconda_info = risposte_seconda_info[['chiarezza','utilita','bellezza','intuitivita','informativita','totale']]
     # Calcoliamo ora com'è la correlazione tra la valutazione totale di un utente e la percezione teorica. Mostriamo anche su uno scatter plot la distribuzione.
-    print('-----------------------------------------------------')
 
-    totale = np.array(risposte['totale'])
-    percepito = np.array(risposte['percepito'])
+    '''
+    totale = np.array(risposte_seconda_info['totale'])
+    percepito = np.array(risposte_seconda_info['percepito'])
     r = round((np.corrcoef(totale, percepito)[0, 1])**2, 3)
 
     
     print("L'indice r^2 vale", r)
-
+    '''
     # Disegno tutti i grafici.
+    box_plot_1(dati_prima_info, out_dir)
+    box_plot_2(dati_seconda_info, out_dir)
 
-    #box_plot(dati)
-    violin_plot(dati, out_dir)
-    #scatter_plot(risposte['totale'], risposte['percepito'])
-    #plot_corr(risposte)
-    correlation_plot(risposte, out_dir)
+    scatter_plot_1(risposte_prima_info['totale'], risposte_prima_info['percepito'], out_dir)
+    scatter_plot_2(risposte_seconda_info['totale'], risposte_seconda_info['percepito'], out_dir)
+
+    correlation_plot(risposte_prima_info, out_dir)
+
     
 
     
--- quality/times.py
+++ quality/times.py
@@ -5,43 +5,20 @@ da capire se sono di immediata lettura.
 
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
 import seaborn as sns
 
-def violin_plot(data, out_dir):
+def box_plot(data, out_dir):
     '''
-    Disegna il violin plot dei dati
+    Disegna il box plot dei dati
     @params:
         data:   Dati di cui disegnare i violin plot.
     '''
-    fig, ax= plt.subplots(figsize=(15, 8))
-
-    bp = ax.violinplot(data, showmeans=True, showmedians=True,
-        showextrema=True)
-    ax.yaxis.grid(True, linestyle='-', which='major', color='lightgrey',
-               alpha=0.5)
-
-    pos   = [1, 2, 3]
-    label = ['task_1', 'task_2', 'task_3']
-    ax.set_axisbelow(True)
-    ax.set_title('Esecuzione dei task')
-    ax.set_xlabel('Task')
-    ax.set_ylabel('Valori')
-    ax.set_ylim(0,6.5)
-    ax.set_xticks(pos)
-    ax.set_xticklabels(label)
-
-
-    colors = [(0.2,0.1,0.3), (0.9,0.8,0.4), (0.2,0.3,0.7)]
-
-    for pc, color in zip(bp['bodies'],colors):
-        pc.set_facecolor(color)
-        pc.set_edgecolor(color)
-        pc.set_alpha(1)
-
+    data = data.melt(var_name='Tasks', value_name='Times')
+    fig = plt.subplots(figsize=(15, 8))
+    sns.boxplot(x="Tasks", y="Times",data = data, palette="Set3")
+    plt.grid()
     plt.show()
-    fig.savefig(out_dir + '/tempi_violin_plot.png', bbox_inches='tight', dpi = 600)
-
+    plt.savefig(out_dir + '/tempi_box_plot_seaborn.png', bbox_inches='tight', dpi = 600)
 
 def times(out_dir):
     '''
@@ -49,21 +26,14 @@ def times(out_dir):
     '''
     tempi = pd.read_csv("tempi.csv")
 
-    # Rename the columns.
-
-    tempi.columns = ['id_persona', 'task_1', 'task_2', 'task_3']
-
-    print(tempi)
-
     # Creates the new summarize column
     
     print('-----------------------------------------------------')
     a = pd.DataFrame(round(tempi.describe(),2))
     print(a)
-    dati = [tempi['task_1'], tempi['task_2'], tempi['task_3']]
 
     # Calcoliamo ora com'è la correlazione tra la valutazione totale di un utente e la percezione teorica. Mostriamo anche su uno scatter plot la distribuzione.
     print('-----------------------------------------------------')
 
     # Disegno tutti i grafici.
-    violin_plot(dati, out_dir)
+    box_plot(tempi,out_dir)
